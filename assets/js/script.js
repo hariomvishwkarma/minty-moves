7a57df5d0cbfd54b732b796f3c04e304f833cbd6
hariomvishwakarma1 <117162788+hariomvishwkarma@users.noreply.github.com>
2023-08-22 18:38:41 +0530

    sixth section complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,3 +122,69 @@ const destinationSwiper = new Swiper('.destination-section .swiper', {
     },
 });
 // });
+
+
+$(document).ready(function() {
+    $('.second-sec-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: true,
+        speed: 1000,
+        infinite: true,
+        autoplaySpeed: 5000,
+        autoplay: false,
+        nextArrow: '<svg class="slick-arrow nextArrow" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none"><circle opacity="0.1" cx="20" cy="20" r="20" transform="matrix(-1 0 0 1 40 0)" fill="black"/><path d="M17.6031 14.4L23.2031 20.8L17.6031 26" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>',
+        prevArrow: '<svg class="slick-arrow prevArrow" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none"><circle opacity="0.1" cx="20" cy="20" r="20" fill="black"/><path d="M22.3969 14.4L16.7969 20.8L22.3969 26" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 596,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
+
+$(document).ready(function() {
+    $('.fourth-sec-tab-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: true,
+        speed: 1000,
+        infinite: true,
+        autoplaySpeed: 5000,
+        autoplay: false,
+        // nextArrow: '<svg class="slick-arrow nextArrow" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none"><circle opacity="0.1" cx="20" cy="20" r="20" transform="matrix(-1 0 0 1 40 0)" fill="black"/><path d="M17.6031 14.4L23.2031 20.8L17.6031 26" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>',
+        // prevArrow: '<svg class="slick-arrow prevArrow" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40" fill="none"><circle opacity="0.1" cx="20" cy="20" r="20" fill="black"/><path d="M22.3969 14.4L16.7969 20.8L22.3969 26" stroke="white" stroke-width="2" stroke-linecap="round"/></svg>',
+        responsive: [{
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 3,
+                }
+            }, {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 596,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
+});
